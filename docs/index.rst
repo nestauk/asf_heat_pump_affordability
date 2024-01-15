8db12b339ad275e0b676602c54e1c5d77172648e
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-01-15 13:56:44 +0000

    Setup Nesta Data Science cookiecutter

---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/asf_heat_pump_affordability
+   License <license>
+
+asf_heat_pump_affordability
+===============================
+
+Estimate heat pump costs across different housing archetypes
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,26 @@
+"""asf_heat_pump_affordability."""
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+BASE_DIR = Path(__file__).parent
+
+
+setup(
+    name="asf_heat_pump_affordability",
+    long_description=open(BASE_DIR / "README.md").read(),
+    install_requires=read_lines(BASE_DIR / "requirements.txt"),
+    extras_require={"dev": read_lines(BASE_DIR / "requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="Estimate heat pump costs across different housing archetypes",
+    author="Nesta",
+    license="MIT",
+)
