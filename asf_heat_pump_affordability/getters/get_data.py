f81c28c5b3742f8a3569b3b11d3751bd31949eda
roisin <104171770+crispy-wonton@users.noreply.github.com>
2024-01-30 16:38:15 +0000

    add docstrings to get_data functions and refactor code

--- asf_heat_pump_affordability/getters/get_data.py
+++ asf_heat_pump_affordability/getters/get_data.py
@@ -4,7 +4,8 @@ from io import BytesIO
 from zipfile import ZipFile
 from typing import Optional
 import time
-from asf_heat_pump_affordability import config
+
+from asf_heat_pump_affordability import config, json_schema
 
 
 def get_df_from_url(
@@ -25,7 +26,7 @@ def get_df_from_url(
     if file_type == "csv":
         df = pd.read_csv(BytesIO(res.content), **kwargs)
     elif file_type == "zip":
-        df = pd.read_csv(ZipFile(BytesIO(res.content)).open(file_name))
+        df = pd.read_csv(ZipFile(BytesIO(res.content)).open(file_name), **kwargs)
     else:
         df = pd.read_excel(BytesIO(res.content), **kwargs)
 
@@ -34,129 +35,118 @@ def get_df_from_url(
 
 def get_list_off_gas_postcodes(sheet_name: str = "Off-Gas Postcodes 2023") -> list:
     """
-    Get series of off-gas postcodes.
+    Get list of off-gas postcodes in Great Britain.
+
+    Args
+        sheet_name (str): name of sheet where off-gas postcode data is stored in file
+
+    Returns
+        list: off-gas postcodes in Great Britain
     """
     df = get_df_from_url(
-        url=config["data_source"]["off_gas_postcodes_url"],
-        file_type="csv",
+        url=config["data_source"]["gb_off_gas_postcodes_url"],
         sheet_name=sheet_name,
     )
 
-    off_gas_postcodes = df["Post Code"].str.replace(" ", "").to_list()
+    off_gas_postcodes_list = df["Post Code"].str.replace(" ", "").to_list()
 
-    return off_gas_postcodes
+    return off_gas_postcodes_list
 
 
-def get_dict_rural_class_ew():
+def get_df_rural_class_engwal(sheet_name: str = "OA11") -> pd.DataFrame:
     """
-    Get dict of dataframes with rural urban classification information for areas at different geographic levels.
+    Get dataframe with rural urban classification information by area for England and Wales.
+
+    Args
+        sheet_name (str): name of sheet where rural/urban classification information stored for chosen geographic level.
+                          Default "OA11" to get rural/urban classification by output area.
+
+    Returns
+        pd.DataFrame: rural urban classification by area for England and Wales
     """
-    df_dict = get_df_from_url(
-        config["data_source"]["rural_urban_classification_2011_url"],
+    df = get_df_from_url(
+        config["data_source"]["engwal_rural_urban_classification_2011_url"],
         engine="odf",
-        sheet_name=None,
+        sheet_name=sheet_name,
         skiprows=2,
     )
 
-    for sheet, df in df_dict.items():
-        df.columns = [
-            f"{sheet}_{'_'.join(col.lower().split(' '))}" for col in df.columns
-        ]
-
-    return df_dict
-
-
-def _get_df_postcode_to_area_ew():
-    """
-    Get postcode to area look-up table.
-    """
-    df = get_df_from_url(
-        config["data_source"]["postcode_to_area_lookup_2011_url"],
-        file_type="csv",
-        encoding="latin-1",
-        compression="zip",
-        header=0,
-        sep=",",
-        quotechar='"',
-    )
+    df.columns = [
+        f"{sheet_name}_{'_'.join(col.lower().split(' '))}" for col in df.columns
+    ]
 
     return df
 
 
-def get_df_rural_class_s():
+def get_df_postcode_to_area_engwal(postcode_col: str = "PCD7") -> pd.DataFrame:
     """
-    Get Scottish postcode directory df.
+    Get postcode to area look-up table for England and Wales.
+
+    Args
+        postcode_col (str): name of column containing postcodes
+
+    Returns
+        pd.DataFrame: postcode to area look-up for England and Wales
     """
-    rural_df_list = []
-
-    for file_name in ["SmallUser.csv", "LargeUser.csv"]:
-        df = get_df_from_url(
-            config["data_source"]["scottish_postcode_directory_2020_url"],
-            file_type="zip",
-            file_name=file_name,
-            encoding="latin-1",
-        )
-        df["scot_rural_urban_classification_2fold"] = df[
-            "UrbanRural8Fold2020Code"
-        ].apply(lambda x: 1 if x <= 5 else 2)
-        df["postcode"] = df["Postcode"].str.replace(" ", "")
-        df = df[["postcode", "scot_rural_urban_classification_2fold"]]
-        rural_df_list.append(df)
-        time.sleep(2)
-
-    rural_class_scotland_df = pd.concat([rural_df_list[0], rural_df_list[1]])
-
-    return rural_class_scotland_df
-
-
-def generate_df_postcode_to_rural_class(
-    postcode_to_area_df,
-    rural_df_dict,
-    rural_sheet_names=["OA11", "LSOA11", "MSOA11"],
-    pcd_join_keys=["OA11CD", "LSOA11CD", "MSOA11CD"],
-    rural_class_variable="rural_urban_classification_2011_(2_fold)",
-    postcode_col="PCD7",
-):
-    """
-    Join rurality and postcode to area dataframes.
-    """
-    df = postcode_to_area_df.copy()
-    df = _join_df_postcode_to_rural_class(
-        df, rural_df_dict, rural_sheet_names, pcd_join_keys
+    df = get_df_from_url(
+        config["data_source"]["engwal_postcode_to_area_lookup_2011_url"],
+        file_type="csv",
+        encoding="latin-1",
+        compression="zip",
+        dtype=json_schema["engwal_postcode_to_area_lookup_2011_data"],
     )
-    df[rural_class_variable] = _fill_cols(
-        df=df,
-        series=df[f"{rural_sheet_names[0]}_{rural_class_variable}"],
-        variable=rural_class_variable,
-        rural_sheet_names=rural_sheet_names,
-    )
+
     df["postcode"] = df[postcode_col].str.replace(" ", "")
 
     return df
 
 
-def _join_df_postcode_to_rural_class(
-    df, rural_df_dict, rural_sheet_names, pcd_join_keys
-):
+def get_df_rural_class_sct() -> pd.DataFrame:
     """
-    Join each rural classification sheet to postcode to area dataframe
+    Get rural/urban classification information by postcode for Scotland.
+
+    Returns
+        pd.DataFrame: rural/urban classification by postcode for Scotland
     """
-    for sheet, pcd_join in zip(rural_sheet_names, pcd_join_keys):
-        rural_df = rural_df_dict[sheet]
-        rural_join = rural_df.columns[0]
-        df = df.merge(rural_df, how="left", left_on=pcd_join, right_on=rural_join)
+    schemas = [
+        json_schema["scottish_postcode_directory_2020_small_user_data"],
+        json_schema["scottish_postcode_directory_2020_large_user_data"],
+    ]
+    rural_df_list = []
+
+    for file_name, schema in zip(["SmallUser.csv", "LargeUser.csv"], schemas):
+        df = get_df_from_url(
+            config["data_source"]["scottish_postcode_directory_2020_url"],
+            file_type="zip",
+            file_name=file_name,
+            encoding="latin-1",
+            dtype=schema,
+        )
+        df = _get_df_rural_class_2fold_sct(df)
+        rural_df_list.append(df)
+        time.sleep(2)
+
+    df = pd.concat([rural_df_list[0], rural_df_list[1]])
 
     return df
 
 
-def _fill_cols(
-    df, series, variable, rural_sheet_names
-):  # TODO: only works effectively when rural_sheet_names in order of OA size
+def _get_df_rural_class_2fold_sct(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Fill na values in specified rural urban classification column
-    """
-    cols = [f"{sheet_name}_{variable}" for sheet_name in rural_sheet_names]
-    for col in cols:
-        series = series.fillna(df[col])
+    Get 2-fold rural/urban classification for Scotland by postcode.
 
-    return series
+    Args
+        df (pd.DataFrame): dataframe containing rural/urban classification information by postcode for Scotland
+
+    Returns
+        pd.DataFrame: dataframe with 2-fold rural/urban classification by postcode for Scotland
+    """
+    df["sct_rural_urban_class_2fold"] = df["UrbanRural8Fold2020Code"].apply(
+        lambda x: "Urban" if x <= 5 else "Rural"
+    )
+    df["postcode"] = df["Postcode"].str.replace(" ", "")
+    df = df.rename(columns={"OutputArea2011Code": "OA11CD"})[
+        ["postcode", "OA11CD", "sct_rural_urban_class_2fold"]
+    ]
+
+    return df
